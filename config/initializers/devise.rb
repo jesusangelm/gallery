365f82412b4ad272b58f8b00195a19770de24d8a
Jesus Marin <jesusangelm@gmail.com>
2015-04-20 16:31:14 -0430

    setup devise secret_key on .env file

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,6 +5,7 @@ Devise.setup do |config|
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
   # config.secret_key = 'd3ebc0abc2ac95780bcfb7a3a5b7d7e7f4fb3329bf08ca04a451d8d3dcc3cdb81cda385fe5a537608c8e8751ca61ac256fa0407a0dac1c814ab59039ec58a4c4'
+  config.secret_key = ENV["DEVISE_SECRET_KEY"]
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
